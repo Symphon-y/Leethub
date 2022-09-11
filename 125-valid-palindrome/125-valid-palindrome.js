15898488aba3518e191fc00e541f9f2e0bc6adcb
Travis Redden <travisredden@outlook.com>
2022-09-11 07:32:36 -0500

    Time: 124 ms (35.33%), Space: 44.4 MB (75.45%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.js
+++ 125-valid-palindrome/125-valid-palindrome.js
@@ -6,7 +6,6 @@ var isPalindrome = function (s) {
   let formatted = s
     .toLowerCase()
     .replace(/[\W_]+/g, '')
-    .split('');
   let left = 0;
   let right = formatted.length - 1;
   while (right > left) {
