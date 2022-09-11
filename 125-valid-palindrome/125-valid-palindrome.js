6f78dbf39b5c073bbc789aaa57644429c23bfe00
Travis Redden <travisredden@outlook.com>
2022-09-11 07:30:58 -0500

    Time: 124 ms (35.33%), Space: 45 MB (62.35%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.js
+++ 125-valid-palindrome/125-valid-palindrome.js
@@ -3,21 +3,18 @@
  * @return {boolean}
  */
 var isPalindrome = function (s) {
-  let result = true;
   let formatted = s
     .toLowerCase()
     .replace(/[\W_]+/g, '')
     .split('');
-  let copy = formatted.map((letter) => {
-    return letter;
-  });
-
-  let reversed = copy.reverse();
-
-  for (let i = 0; i < formatted.length; i++) {
-    if (formatted[i] !== reversed[i]) {
-      result = false;
+  let left = 0;
+  let right = formatted.length - 1;
+  while (right > left) {
+    if (formatted[left] !== formatted[right]) {
+      return false;
     }
+    left++;
+    right--;
   }
-  return result;
+  return true;
 };
