435c7ae1e3e6a1bf12ef01c814d57c28460e93b4
Travis Redden <travisredden@outlook.com>
2022-09-12 11:30:36 -0500

    Time: 67 ms (94.41%), Space: 43.1 MB (33.49%) - LeetHub

---
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (numbers, target) {
+  let result = [];
+  let objCache = new Map();
+
+  numbers.forEach((num, index) => {
+    objCache.set(num, index);
+  });
+
+  for (let i = 0; i < numbers.length; i++) {
+    if (objCache.has(target - numbers[i])) {
+      result.push(i + 1, objCache.get(target - numbers[i]) + 1);
+      return result;
+    }
+  }
+  return result;
+};
