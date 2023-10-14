42d7390537ea644c950d9c498e9a865908a602ea
Travis Redden <travisredden@outlook.com>
2023-10-13 20:29:51 -0500

    Time: 206 ms (5.41%), Space: 53.7 MB (5.05%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.ts
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.ts
@@ -25,25 +25,16 @@ const findMedianSortedArrays = (
   let result: number = 0;
   // Merge the arrays into a single sorted array
   const mergedArray = mergeAndSortTwoArrays<number>(nums1, nums2);
-  console.log('mergedArray', mergedArray);
   // If the array length is odd, the median is the middle data point
   if (mergedArray.length % 2 === 1) {
     result = mergedArray[(mergedArray.length - 1) / 2];
-    console.log('mergedArray.length - 1 / 2]', (mergedArray.length - 1) / 2);
-    console.log(mergedArray[(mergedArray.length - 1) / 2]);
-    console.log('result,', result);
   }
   // if the array length is even, the median is the average of the two middle data points.
   else if (mergedArray.length % 1 === 0) {
-    console.log('merged array length', mergedArray.length);
     const midPoint = Math.floor(mergedArray.length / 2);
-    console.log('midPoint', midPoint);
     const leftMiddleIndex: number = midPoint;
-    console.log('leftMiddleIndex', leftMiddleIndex);
     const rightMiddleIndex: number = midPoint - 1;
-    console.log('rightMiddleIndex', rightMiddleIndex);
     result = (mergedArray[leftMiddleIndex] + mergedArray[rightMiddleIndex]) / 2;
-    console.log('result', result);
   }
   return result;
 };
